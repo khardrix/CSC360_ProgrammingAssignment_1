ce60b4e352800d2fb81482946c51fc01ec86a574
khardrix <43048160+khardrix@users.noreply.github.com>
2019-05-19 09:47:00 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -184,30 +184,90 @@ public class Main {
 
         try{
             MyDate date1 = new MyDate("12/31/1979");
+            System.out.println("Date 1: " + date1);
+            System.out.println("Date 1 Month: " + date1.getMonth());
+            System.out.print("Incrementing Date 1 (" + date1 + ") by one day: ");
+            date1.increment();
+            System.out.println(date1);
+            System.out.print("Incrementing Date 1 (" + date1 + ") by one day: ");
+            date1.increment();
+            System.out.println(date1 + "\n");
+
             MyDate date2 = new MyDate("10/21/1980");
-            MyDate date3 = new MyDate("09/11/1998");
-            MyDate date4 = new MyDate();
-            MyDate date5 = new MyDate("01/11/1998");
-            MyDate date6 = new MyDate("02/11/1998");
-            MyDate date7 = new MyDate("02/11/1998");
-
-            System.out.println(date1.getMonth());
-
-            System.out.println(date1);
+            System.out.println("Date 2: " + date2);
+            System.out.println("Date 2 Month: " + date2.getMonth());
+            System.out.print("Incrementing Date 2 (" + date2 + ") by one day: ");
+            date2.increment();
             System.out.println(date2);
-            System.out.println(date3);
-            System.out.println(date4);
-            System.out.println(date5);
-            System.out.println(date6);
+            System.out.print("Incrementing Date 2 (" + date2 + ") by one day: ");
+            date2.increment();
+            System.out.println(date2 + "\n");
 
+            System.out.print("Date 1 compared to Date 2: ");
             date1.compareTo(date2);
+            System.out.println("___________________________________________________________________");
 
-            date1.increment();
-            System.out.println(date1);
 
-            date1.increment();
-            System.out.println(date1);
-            
+            MyDate date3 = new MyDate("09/11/1998");
+            System.out.println("\nDate 3: " + date3);
+            System.out.println("Date 3 Month: " + date3.getMonth());
+            System.out.print("Incrementing Date 3 (" + date3 + ") by one day: ");
+            date3.increment();
+            System.out.println(date3);
+            System.out.print("Incrementing Date 3 (" + date3 + ") by one day: ");
+            date3.increment();
+            System.out.println(date3 + "\n");
+
+            MyDate date4 = new MyDate();
+            System.out.println("Date 4: " + date4);
+            System.out.println("Date 4 Month: " + date4.getMonth());
+            System.out.print("Incrementing Date 4 (" + date4 + ") by one day: ");
+            date4.increment();
+            System.out.println(date4);
+            System.out.print("Incrementing Date 4 (" + date4 + ") by one day: ");
+            date4.increment();
+            System.out.println(date4 + "\n");
+
+            System.out.print("Date 3 compared to Date 4: ");
+            date3.compareTo(date4);
+            System.out.println("___________________________________________________________________");
+
+
+            MyDate date5 = new MyDate("01/11/1998");
+            System.out.println("\nDate 5: " + date5);
+            System.out.println("Date 5 Month: " + date5.getMonth());
+            System.out.print("Incrementing Date 5 (" + date5 + ") by one day: ");
+            date5.increment();
+            System.out.println(date5);
+            System.out.print("Incrementing Date 5 (" + date5 + ") by one day: ");
+            date5.increment();
+            System.out.println(date5 + "\n");
+
+            MyDate date6 = new MyDate("02/11/1998");
+            System.out.println("Date 6: " + date6);
+            System.out.println("Date 6 Month: " + date6.getMonth());
+            System.out.print("Incrementing Date 6 (" + date6 + ") by one day: ");
+            date6.increment();
+            System.out.println(date6);
+            System.out.print("Incrementing Date 6 (" + date6 + ") by one day: ");
+            date6.increment();
+            System.out.println(date6 + "\n");
+
+            System.out.print("Date 5 compared to Date 6: ");
+            date5.compareTo(date6);
+            System.out.println("___________________________________________________________________");
+
+
+            MyDate date7 = new MyDate("02/29/1998");
+            System.out.println("\nDate 7: " + date6);
+            System.out.println("Date 7 Month: " + date7.getMonth());
+            System.out.print("Incrementing Date 7 (" + date7 + ") by one day: ");
+            date7.increment();
+            System.out.println(date7);
+            System.out.print("Incrementing Date 7 (" + date7 + ") by one day: ");
+            date7.increment();
+            System.out.println(date7 + "\n");
+
         } catch (IllegalDateException e){
             System.out.println(e);
         }
